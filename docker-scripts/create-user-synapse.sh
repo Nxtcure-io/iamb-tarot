8be9e2c198f7c08f3ba341f8ef27f3267e2f3a1f
rivak08 <Thekajangu@gmail.com>
2025-10-06 19:53:52 -0500

    adding all test users

--- docker-scripts/create-user-synapse.sh
+++ docker-scripts/create-user-synapse.sh
@@ -1,1 +1,3 @@
 docker exec -it synapse register_new_matrix_user http://localhost:8008 -c /data/homeserver.yaml -u Riva -p Test123$ -a
+docker exec -it synapse register_new_matrix_user http://localhost:8008 -c /data/homeserver.yaml -u Keanu -p Test123$ -a
+docker exec -it synapse register_new_matrix_user http://localhost:8008 -c /data/homeserver.yaml -u Waqaas -p Test123$ -a
